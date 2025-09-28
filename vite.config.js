381bd1d38571d81f9bfb72822c00dd9d27177e8c
costabusinessservice-alt <costabusinessservice@gmail.com>
2025-09-28 01:18:48 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/costa-business-project/', // <- importante para GH Pages
+  base: process.env.NODE_ENV === "production" 
+    ? "/costa-business-project/" 
+    : "/",
 })
