f4d5001df0c827a87e1af6613f88e66db77aedc3
costabusinessservice-alt <costabusinessservice@gmail.com>
2025-09-28 00:44:44 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import AboutUs from "./pages/AboutUs";
@@ -11,32 +11,34 @@ import FreeQuote from "./pages/FreeQuote";
 
 export default function App() {
   return (
-    <div>
-      {/* Navbar */}
-      <nav className="p-4 bg-gray-800 text-white flex gap-6">
-        <Link to="/">Home</Link>
-        <Link to="/services">Services</Link>
-        <Link to="/about">About Us</Link>
-        <Link to="/areas">Service Areas</Link>
-        <Link to="/gallery">Gallery</Link>
-        <Link to="/reviews">Reviews</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/owner">About Owner</Link>
-        <Link to="/quote">Free Quote</Link>
-      </nav>
+    <BrowserRouter basename="/costa-business-project">
+      <div>
+        {/* Navbar */}
+        <nav className="p-4 bg-gray-800 text-white flex gap-6">
+          <Link to="/">Home</Link>
+          <Link to="/services">Services</Link>
+          <Link to="/about">About Us</Link>
+          <Link to="/areas">Service Areas</Link>
+          <Link to="/gallery">Gallery</Link>
+          <Link to="/reviews">Reviews</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/owner">About Owner</Link>
+          <Link to="/quote">Free Quote</Link>
+        </nav>
 
-      {/* Rotas */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/areas" element={<ServiceAreas />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/reviews" element={<Reviews />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/owner" element={<AboutOwner />} />
-        <Route path="/quote" element={<FreeQuote />} />
-      </Routes>
-    </div>
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/areas" element={<ServiceAreas />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/reviews" element={<Reviews />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/owner" element={<AboutOwner />} />
+          <Route path="/quote" element={<FreeQuote />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
